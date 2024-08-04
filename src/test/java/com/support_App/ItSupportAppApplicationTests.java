66624e413c5a1a5bcbd54cc4f69f9699b79672cd
imane-el-mazouz <elmazouz.imane@gmail.com>
2024-08-04 18:50:41 +0000

    configure prj spring boot

---
+++ src/test/java/com/support_App/ItSupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.support_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
